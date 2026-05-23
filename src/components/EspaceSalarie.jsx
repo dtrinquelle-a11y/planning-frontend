9f4772ab85a948361c05b45be357f9eb4f27ecf8
dtrinquelle-a11y <dtrinquelle@gmail.com>
2026-05-23 18:56:52 +0200

    fix eslint EspaceSalarie

--- src/components/EspaceSalarie.jsx
+++ src/components/EspaceSalarie.jsx
@@ -44,7 +44,7 @@ export default function EspaceSalarie() {
   const [weekOffset, setWeekOffset] = useState(0);
   const [shifts, setShifts] = useState([]);
   const [modulation, setModulation] = useState(null);
-  const [echanges, setEchanges] = useState([]);
+  const [echanges] = useState([]);
   const [echangeModal, setEchangeModal] = useState(false);
   const [echangeForm, setEchangeForm] = useState({ shift_id: '', message: '' });
   const [toast, setToast] = useState('');

2931f91dcc43cf2056c9a1d39ab511612838e565
dtrinquelle-a11y <dtrinquelle@gmail.com>
2026-05-30 09:45:35 +0200

    ajout timeline planning transversal

--- src/App.js
+++ src/App.js
@@ -115,6 +115,7 @@ export default function App() {
       {page === 'pointage' && <Pointeuse employeeId={profile?.employee_id} employeeName={profile?.employees ? profile.employees.first_name + ' ' + profile.employees.last_name : ''} />}
       {page === 'ged' && <GED isManager={isManager} />}
       {page === 'ged' && <GED isManager={isManager} />}
+      {page === 'timeline' && <Timeline />}
     </div>
   );
 }

79b739d81c5fb7e94b96b4a8ac3294f3336c5ed0
dtrinquelle-a11y <dtrinquelle@gmail.com>
2026-08-01 20:17:14 +0200

    fix import Parametrage

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import GED from './components/GED';
 import Timeline from './components/Timeline';
 import Onboarding from './components/Onboarding';
 import DossiersRH from './components/DossiersRH';
+import Parametrage from './components/Parametrage';
 
 function AppInner() {
   const { colors: C, darkMode, toggle } = useTheme();
